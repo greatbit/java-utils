5c8d8b826ee0a62b3d3b668fe3d630a7618ecbc4
azee <azee@yandex-team.ru>
2014-04-11 18:51:22 +0400

    Update XML unmarhsaller test

--- src/test/java/com/azee/utils/marshall/xml/XMLUnmarshallerTest.java
+++ src/test/java/com/azee/utils/marshall/xml/XMLUnmarshallerTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.ByteArrayInputStream;
 
 import static junit.framework.Assert.assertNotNull;
 import static org.hamcrest.core.Is.is;
@@ -20,11 +19,9 @@ import static org.hamcrest.core.Is.is;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:spring-test-context.xml")
 public class XMLUnmarshallerTest {
+
     @Autowired
     XMLUnmarshaller xmlUnmarshaller;
-//
-//    private String marshalledBean = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
-//            "<namespaceBeanExample><value>2</value></namespaceBeanExample>";
 
     private String marshalledBean = "<beanExample><value>2</value></beanExample>";
 
@@ -50,7 +47,7 @@ public class XMLUnmarshallerTest {
     public void testUnmarshallWithoutNamespace() throws Exception {
         BeanWithNamespaceExample bean = xmlUnmarshaller.unmarshall(marshalledBean, BeanWithNamespaceExample.class);
         assertNotNull(bean);
-        Assert.assertThat("Wrong bean value", bean.getValue(), is(10));
+        Assert.assertThat("Wrong bean value", bean.getValue(), is(2));
     }
 
     @Test
