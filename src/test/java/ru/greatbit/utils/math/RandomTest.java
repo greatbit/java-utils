23245a510c4038973a525ab7f7bb0db18158fffb
Aziz Namazov <azee@77.88.2.156-spb.dhcp.yndx.net>
2015-06-23 11:31:02 +0300

    Random test

--- src/test/java/ru/greatbit/utils/math/RandomTest.java
+++ src/test/java/ru/greatbit/utils/math/RandomTest.java
@@ -1,7 +1,24 @@
 package ru.greatbit.utils.math;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by azee on 23.06.15.
  */
 public class RandomTest {
+
+    @Test
+    public void testGetNext(){
+        final int iterations = 100;
+        int sum = 0;
+        for (int i = 0; i <= iterations; i++){
+            int rand = Random.next(1, 10);
+            assertThat(rand, both(greaterThanOrEqualTo(1)).and(lessThanOrEqualTo(10)));
+            sum += rand;
+        }
+        assertThat(sum, both(greaterThan(0)).and(lessThan(10 * iterations)));
+    }
 }
