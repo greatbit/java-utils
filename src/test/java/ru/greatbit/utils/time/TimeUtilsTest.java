96ebd6b3d260b6b5f7c57cd33bf033bcbf7ae92b
anamazov <Aziz.Namazov@macquarie.com>
2018-12-20 10:39:06 +1100

    Add time date test

--- src/test/java/ru/greatbit/utils/time/TimeUtilsTest.java
+++ src/test/java/ru/greatbit/utils/time/TimeUtilsTest.java
@@ -1,6 +1,8 @@
 package ru.greatbit.utils.time;
 
 import org.junit.Test;
+
+import java.util.Date;
 import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
@@ -18,10 +20,19 @@ public class TimeUtilsTest {
     public void getStartOfTheDayTest(){
         TimeZone.setDefault(TimeZone.getTimeZone("Etc/Greenwich"));
         TimeZone tz = TimeZone.getDefault();
-        System.out.println(tz.getRawOffset());
         assertEquals(1389312000000L - tz.getRawOffset(), TimeUtils.getStartOfTheDay(1389358493087L));
         assertEquals(1389312000000L, TimeUtils.getStartOfTheDay(1389358493087L));
         assertEquals(10800000L, TimeUtils.getStartOfTheDay(0));
         assertEquals(10800000L, TimeUtils.getStartOfTheDay(10800000L));
+    }
+
+    @Test
+    public void getStartOfTheDayDateTest(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Etc/Greenwich"));
+        TimeZone tz = TimeZone.getDefault();
+        assertEquals(new Date(1389312000000L - tz.getRawOffset()), TimeUtils.getStartOfTheDay(new Date(1389358493087L)));
+        assertEquals(new Date(1389312000000L), TimeUtils.getStartOfTheDay(new Date(1389358493087L)));
+        assertEquals(new Date(0), TimeUtils.getStartOfTheDay(new Date(0)));
+        assertEquals(new Date(0), TimeUtils.getStartOfTheDay(new Date(10800000L)));
     }
 }
